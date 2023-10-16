73893f9735dd0644536abd3e7893921a940cf4b0
abylay-11 <113657296+abylay-11@users.noreply.github.com>
2023-10-16 17:57:11 +0500

    Lesson 5. Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -22,9 +24,7 @@ class CryptoCurrenciesListApp extends StatelessWidget {
               titleTextStyle: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w700
-              )
-          ),
+                  fontWeight: FontWeight.w700)),
           listTileTheme: const ListTileThemeData(iconColor: Colors.white),
           textTheme: TextTheme(
               bodyMedium: const TextStyle(
@@ -35,21 +35,26 @@ class CryptoCurrenciesListApp extends StatelessWidget {
                   color: Colors.white.withOpacity(0.6),
                   fontWeight: FontWeight.w700,
                   fontSize: 14))),
-      home: const MyHomePage(title: 'Crypto Currencies List'),
+      routes: {
+        '/': (context) => const CryptoListScreen(
+              title: 'Crypto Currencies List',
+            ),
+        '/coin': (context) => const CryptoCoinScreen()
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CryptoListScreenState extends State<CryptoListScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -62,19 +67,64 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ListView.separated(
           separatorBuilder: (context, i) => const Divider(),
           itemCount: 10,
-          itemBuilder: (context, i) => ListTile(
-            leading: SvgPicture.asset(
-              'assets/svg/bitcoin_logo.svg',
-              height: 30,
-              width: 30,
-            ),
-            title: Text('Bitcoin', style: theme.textTheme.bodyMedium),
-            subtitle: Text(
-              '20000\$',
-              style: theme.textTheme.labelSmall,
-            ),
-            trailing: const Icon(Icons.arrow_forward_ios),
-          ),
+          itemBuilder: (context, i) {
+            const coinName = 'Bitcoin';
+            return ListTile(
+              leading: SvgPicture.asset(
+                'assets/svg/bitcoin_logo.svg',
+                height: 30,
+                width: 30,
+              ),
+              title: Text(coinName, style: theme.textTheme.bodyMedium),
+              subtitle: Text(
+                '20000\$',
+                style: theme.textTheme.labelSmall,
+              ),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () => {
+                Navigator.of(context).pushNamed('/coin', arguments: coinName)
+              },
+            );
+          },
         ));
+  }
+}
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+
+    assert(args != null && args is String, 'You must provide String args');
+    // if (args == null) {
+    //   log('You must provide args');
+    //   return;
+    // }
+    // if (args is! String) {
+    //   log('You must provide String args');
+    //   return;
+    // }
+
+    coinName = args as String;
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(coinName ?? '...'),
+      ),
+    );
   }
 }
