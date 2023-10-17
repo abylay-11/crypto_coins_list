a8409582f4f2c143559523125ed1bee2f3f11904
abylay-11 <113657296+abylay-11@users.noreply.github.com>
2023-10-17 17:30:04 +0500

    Lesson 7. Working with API, http | dio library

--- lib/features/crypto_list/view/crypto_list_screen.dart
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:crypto_coins_list/features/crypto_list/widgets/widgets.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins_repository.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
 
 class CryptoListScreen extends StatefulWidget {
@@ -11,20 +13,37 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
+  List<CryptoCoin>? _cryptoCoinsList;
+
+  @override
+  void initState() {
+    _loadCryptoCoins();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: ListView.separated(
-          separatorBuilder: (context, i) => const Divider(),
-          itemCount: 10,
-          itemBuilder: (context, i) {
-            const coinName = 'Bitcoin';
-            return const CryptoCoinTile(coinName: coinName);
-          },
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: _cryptoCoinsList == null
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              padding: const EdgeInsets.only(top: 16),
+              separatorBuilder: (context, i) => const Divider(),
+              itemCount: _cryptoCoinsList!.length,
+              itemBuilder: (context, i) {
+                final coin = _cryptoCoinsList![i];
+                return CryptoCoinTile(coin: coin);
+              },
+            ),
+    );
+  }
+
+  Future<void> _loadCryptoCoins() async {
+    _cryptoCoinsList = await CryptoConinsRepository().getCoinsList();
+    setState(() {});
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
     useMaterial3: true,
     scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
     dividerColor: Colors.white24,
     appBarTheme: const AppBarTheme(
+        elevation: 0,
         backgroundColor: Color.fromARGB(255, 31, 31, 31),
         titleTextStyle: TextStyle(
             color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700)),
